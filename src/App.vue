5a8184966425702a0743339742b91fc6cb219bf3
Naman Bajpai <namanbajpai4@gmail.com>
2024-02-17 17:01:30 -0500

    design changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&display=swap" rel="stylesheet">
   <div>
     <h4>
-      The current count is {{ count }}; 
+      The current count is
     </h4>
     <h1 id="count">0</h1>
     <button id="add">+</button>
@@ -28,3 +31,29 @@ export default {
   }
 }
 </script>
+<!-- 
+<script setup>
+import { ref } from 'vue'
+
+const open = ref(false)
+</script>
+
+<template>
+  <button @click="open = true">Open Modal</button>
+
+  <div v-if="open" class="modal">
+    <p>Hello from the modal!</p>
+    <button @click="open = false">Close</button>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+}
+</style> -->
