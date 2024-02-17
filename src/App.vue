cb386c7099f4966cf09954d8846f2140912ad037
Naman Bajpai <namanbajpai4@gmail.com>
2024-02-17 16:41:43 -0500

    first design

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h4>
+      The current count is {{ count }}; 
+    </h4>
+    <h1 id="count">0</h1>
+    <button id="add">+</button>
+    <button id="subtract">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  mounted() {
+    document.getElementById('add').addEventListener('click', () => {
+      this.count++
+      document.getElementById('count').innerText = this.count
+    })
+    document.getElementById('subtract').addEventListener('click', () => {
+      this.count--
+      document.getElementById('count').innerText = this.count
+    })
+  }
+}
+</script>
